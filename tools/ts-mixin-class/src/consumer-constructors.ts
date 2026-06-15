0cbd6d709c1a1e49249fda8e5c9c4a95863c298f
tonyboho <294078906+tonyboho@users.noreply.github.com>
2026-06-15 11:32:11 +0300

    refactor(ts-mixin-class): extract consumer constructor helpers

---
+++ tools/ts-mixin-class/src/consumer-constructors.ts
@@ -0,0 +1,70 @@
+import type * as ts from "typescript"
+import {
+    generatedTextRange,
+    preserveTextRange
+} from "./util.js"
+import type { TypeScript } from "./util.js"
+
+export function addSyntheticSuperCallToConstructors(
+    tsInstance: TypeScript,
+    sourceFile: ts.SourceFile,
+    members: ts.NodeArray<ts.ClassElement>,
+    shouldAdd: boolean
+): ts.NodeArray<ts.ClassElement> {
+    if (!shouldAdd) {
+        return members
+    }
+
+    let changed = false
+    const updatedMembers = members.map((member) => {
+        if (!tsInstance.isConstructorDeclaration(member) ||
+            member.body === undefined ||
+            constructorHasSuperCall(tsInstance, member)
+        ) {
+            return member
+        }
+
+        changed = true
+
+        return tsInstance.factory.updateConstructorDeclaration(
+            member,
+            member.modifiers,
+            member.parameters,
+            tsInstance.factory.updateBlock(member.body, [
+                syntheticSuperCall(tsInstance, sourceFile, member),
+                ...member.body.statements
+            ])
+        )
+    })
+
+    return changed
+        ? preserveTextRange(tsInstance, tsInstance.factory.createNodeArray(updatedMembers), members)
+        : members
+}
+
+function constructorHasSuperCall(
+    tsInstance: TypeScript,
+    declaration: ts.ConstructorDeclaration
+): boolean {
+    return declaration.body?.statements.some((statement) => {
+        return tsInstance.isExpressionStatement(statement) &&
+            tsInstance.isCallExpression(statement.expression) &&
+            statement.expression.expression.kind === tsInstance.SyntaxKind.SuperKeyword
+    }) ?? false
+}
+
+function syntheticSuperCall(
+    tsInstance: TypeScript,
+    sourceFile: ts.SourceFile,
+    declaration: ts.ConstructorDeclaration
+): ts.Statement {
+    return preserveTextRange(
+        tsInstance,
+        tsInstance.factory.createExpressionStatement(tsInstance.factory.createCallExpression(
+            tsInstance.factory.createSuper(),
+            undefined,
+            []
+        )),
+        generatedTextRange(sourceFile, declaration.body?.statements.pos ?? declaration.pos)
+    )
+}
--- tools/ts-mixin-class/src/consumer-expand.ts
+++ tools/ts-mixin-class/src/consumer-expand.ts
@@ -9,6 +9,7 @@ import {
     linearizationDiagnosticMessage,
     unsupportedBaseDiagnosticMessage
 } from "./consumer-diagnostics.js"
+import { addSyntheticSuperCallToConstructors } from "./consumer-constructors.js"
 import { rewritePublicOnlyUndefinedInitializers } from "./construction-initializers.js"
 import {
     createConstructionMembers,
@@ -511,70 +512,6 @@ function expandConsumerClassWithLinearizationDiagnostic(
         ) ]
 
     return [ ...emptyBaseClass, baseInterface, baseClass, updatedConsumer ]
-}
-
-function addSyntheticSuperCallToConstructors(
-    tsInstance: TypeScript,
-    sourceFile: ts.SourceFile,
-    members: ts.NodeArray<ts.ClassElement>,
-    shouldAdd: boolean
-): ts.NodeArray<ts.ClassElement> {
-    if (!shouldAdd) {
-        return members
-    }
-
-    let changed = false
-    const updatedMembers = members.map((member) => {
-        if (!tsInstance.isConstructorDeclaration(member) ||
-            member.body === undefined ||
-            constructorHasSuperCall(tsInstance, member)
-        ) {
-            return member
-        }
-
-        changed = true
-
-        return tsInstance.factory.updateConstructorDeclaration(
-            member,
-            member.modifiers,
-            member.parameters,
-            tsInstance.factory.updateBlock(member.body, [
-                syntheticSuperCall(tsInstance, sourceFile, member),
-                ...member.body.statements
-            ])
-        )
-    })
-
-    return changed
-        ? preserveTextRange(tsInstance, tsInstance.factory.createNodeArray(updatedMembers), members)
-        : members
-}
-
-function constructorHasSuperCall(
-    tsInstance: TypeScript,
-    declaration: ts.ConstructorDeclaration
-): boolean {
-    return declaration.body?.statements.some((statement) => {
-        return tsInstance.isExpressionStatement(statement) &&
-            tsInstance.isCallExpression(statement.expression) &&
-            statement.expression.expression.kind === tsInstance.SyntaxKind.SuperKeyword
-    }) ?? false
-}
-
-function syntheticSuperCall(
-    tsInstance: TypeScript,
-    sourceFile: ts.SourceFile,
-    declaration: ts.ConstructorDeclaration
-): ts.Statement {
-    return preserveTextRange(
-        tsInstance,
-        tsInstance.factory.createExpressionStatement(tsInstance.factory.createCallExpression(
-            tsInstance.factory.createSuper(),
-            undefined,
-            []
-        )),
-        generatedTextRange(sourceFile, declaration.body?.statements.pos ?? declaration.pos)
-    )
 }
 
 function createMixinChainExpression(
